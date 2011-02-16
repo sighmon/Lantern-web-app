b01dcf43b99f18b647eaa4077dea3f34b6131b04
sighmon <simon.loffler@gmail.com>
2011-02-17 09:51:25 +1030

    Web app created. When added to home screen on iphone it creates a non-scrolling full-screen app.

---
+++ js/lantern.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+document.addEventListener('touchstart', function(e){ e.preventDefault(); });
+
+document.addEventListener('touchmove', function(e){ e.preventDefault(); });
+
